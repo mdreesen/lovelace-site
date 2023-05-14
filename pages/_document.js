cff06dc146cdc123240c9f5ccc035f2c1f1f6dc2
Michael Dreesen <michaeldreesen90@gmail.com>
2023-05-13 20:12:58 -0600

    tyring another thing

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,8 @@ export default function Document() {
                 <Main />
                 <NextScript  />
             </body>
+
+            <script>{` var exports = {}; `}</script>
         </Html>
     )
 };
